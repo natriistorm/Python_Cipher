6070d713716e9ee2bc3af867d059f27fb5ec9152
natriistorm <natriistorm@mail.ru>
2021-04-06 10:32:20 +0300

    Parsing added

---
+++ parser.py
@@ -0,0 +1,20 @@
+import argparse
+import sys
+
+
+def parser():
+    main_parser = argparse.ArgumentParser()
+    crypt_subparsers = main_parser.add_subparsers(dest='func')
+    cipher_parser = crypt_subparsers.add_parser("Encrypt or Decrypt")
+
+    cipher_parser.add_argument('action', choices=['Encrypt', 'Decrypt'])
+    cipher_parser.add_argument("cipher", choices=['Caesar', 'Vigenere', 'Vernam'],
+                               help='Choose Cipher: Caesar or Vigenere or Vernam')
+    cipher_parser.add_argument('key_file', help='Enter the name of the file with cipher key:'
+                                                'an integer for Caesar and a string for Vigenere and Vernam')
+    cipher_parser.add_argument('input_file', help='Enter the name of file with text to encrypt/decrypt',
+                               default=sys.stdin)
+    cipher_parser.add_argument('--output_file', help='Enter the name of file to put the encrypted/decrypted text',
+                               default=sys.stdout)
+    args = cipher_parser.parse_args()
+    return args
