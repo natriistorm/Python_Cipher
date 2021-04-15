8b51b6c5284a1a1bdf2bd0e5e54f206a1efe4d87
natriistorm <natriistorm@mail.ru>
2021-04-16 00:44:23 +0300

    Переделан запускающий файл

--- commands.py
+++ commands.py
@@ -3,6 +3,7 @@ import text_work
 import caesar
 import vigenere
 import vernam
+import abstract_class
 
 
 def choose_cipher(args, data, keydata):
@@ -11,13 +12,26 @@ def choose_cipher(args, data, keydata):
     elif args.cipher == 'Vigenere':
         output_file = vigenere.vigenere_main(args.action, data, keydata)
     elif args.cipher == 'Vernam':
-        output_file = vernam.vernam_main(args.action, data, keydata)
-    if args.output is not None:
-        text_work.user_output(output_file, args.output)
-    else:
-        text_work.user_output(output_file)
+        output_file = vernam.vernam_main(data, keydata)
+    text_work.user_output(output_file, args.output)
 
-args = parser.parser()
-keydata = text_work.user_input(args.key_file)
-data = text_work.user_input(args.input)
-choose_cipher(args, data, keydata)
+
+def main():
+    args = parser.parser()
+    keydata = text_work.user_input(args.key_file)
+    data = text_work.user_input(args.input)
+    if args.cipher == "Caesar":
+        cipher = caesar.Caesar(keydata, data)
+    elif args.cipher == "Vigenere":
+        cipher = vigenere.Vigenere(keydata, data)
+    elif args.cipher == "Vernam":
+        cipher = vernam.Vernam(keydata, data)
+    if args.action == "Encrypt":
+        result = cipher.encrypt()
+    elif args.action == "Decrypt":
+        result = cipher.decrypt()
+    text_work.user_output(cipher.output_text, args.output)
+
+
+if __name__ == "__main__":
+    main()
