9ad856918a1b5bf8b6c86bdca620daca3cf3c5f1
natriistorm <natriistorm@mail.ru>
2021-04-20 18:40:40 +0300

    Train mode added

--- commands.py
+++ commands.py
@@ -3,30 +3,34 @@ import text_work
 import caesar
 import vigenere
 import vernam
+import trainer
+import abstract_class
 
 
 def main():
     args = parser.parser()
     keydata = text_work.user_input(args.key_file)
     data = text_work.user_input(args.input)
-
+    cipher = abstract_class.AbstractCipher
     if args.cipher == "Caesar":
         cipher = caesar.Caesar(keydata, data)
     elif args.cipher == "Vigenere":
         cipher = vigenere.Vigenere(keydata, data)
     elif args.cipher == "Vernam":
         cipher = vernam.Vernam(keydata, data)
-
-    if args.action == "Encrypt":
-        cipher.encrypt()
-    elif args.action == "Decrypt":
-        cipher.decrypt()
-    if args.action == "Hack":
-        if args.cipher == "Caesar":
-            cipher.hack("json_text.txt")
-        else:
-            raise ValueError("Only Caesar cipher can be hacked!")
-    text_work.user_output(cipher.output_text, args.output)
+    if args.action != "Train":
+        if args.action == "Encrypt":
+            cipher.encrypt()
+        elif args.action == "Decrypt":
+            cipher.decrypt()
+        elif args.action == "Hack":
+            if args.cipher == "Caesar":
+                cipher.hack("text.json")
+            else:
+                raise ValueError("Only Caesar cipher can be hacked!")
+        text_work.user_output(cipher.output_text, args.output)
+    else:
+        trainer.train(data, args.output)
 
 
 if __name__ == "__main__":
--- parser.py
+++ parser.py
@@ -1,20 +1,20 @@
 import argparse
-import sys
 
 
 def parser():
     main_parser = argparse.ArgumentParser()
     crypt_subparsers = main_parser.add_subparsers(dest='func')
-    cipher_parser = crypt_subparsers.add_parser("Encrypt or Decrypt")
+    cipher_parser = crypt_subparsers.add_parser("Action")
 
-    cipher_parser.add_argument('action', choices=['Encrypt', 'Decrypt'])
+    cipher_parser.add_argument('action', choices=['Encrypt', 'Decrypt', 'Hack', 'Train'])
     cipher_parser.add_argument("cipher", choices=['Caesar', 'Vigenere', 'Vernam'],
-                               help='Choose Cipher: Caesar or Vigenere or Vernam')
+                               help='Choose Cipher: Caesar or Vigenere or Vernam', default=None)
     cipher_parser.add_argument('key_file', help='Enter the name of the file with cipher key:'
-                                                'an integer for Caesar and a string for Vigenere and Vernam')
+                                                'an integer for Caesar and a string for Vigenere and Vernam. You don"t '
+                                                'need it for Hack mode', default=None)
     cipher_parser.add_argument('--input', help='Enter the name of file with text to encrypt/decrypt',
                                default=None)
-    cipher_parser.add_argument('--output', help='Enter the name of file to put the encrypted/decrypted text',
-                               default=None)
+    cipher_parser.add_argument('--output', help='Enter the name of file to put the encrypted/decrypted text,'
+                                                'for the Train mode enter the JSON file', default=None)
     args = cipher_parser.parse_args()
     return args
